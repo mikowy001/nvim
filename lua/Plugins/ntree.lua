7c820b793cc6afc4212f411b87b8f1fd19705d56
mikowy001 <mikowy001@gmail.com>
2026-05-21 11:58:31 +0200

    .fs & .vs extension icons mapped to openGL icon

--- lua/Plugins/ntree.lua
+++ lua/Plugins/ntree.lua
@@ -1,13 +1,30 @@
 local nTree = {
-	"nvim-tree/nvim-tree.lua",
-	version = "*",
-	lazy = false,
-	dependencies = {
-    	"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-    	require("nvim-tree").setup {}
-  	end,
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("nvim-web-devicons").setup({
+            override = {
+                vs = {
+                    icon = "",
+                    color = "#56b6c2",
+                    cterm_color = "73",
+                    name = "VertexShader"
+                },
+                fs = {
+                    icon = "",
+                    color = "#56b6c2",
+                    cterm_color = "73",
+                    name = "FragmentShader"
+                }
+            }
+        })
+
+        require("nvim-tree").setup({})
+    end,
 }
 
 return nTree
